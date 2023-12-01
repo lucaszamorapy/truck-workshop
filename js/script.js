725bcbde806f25537e75cdf4c2f67049a127746f
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2023-12-01 00:28:59 -0300

    feat: whatsapp button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 // function MobileMenu() {
 //   const mobileBtn = document.querySelector('.mobile-btn');
 //   const mobileMenu = document.querySelector('.mobile-menu');
@@ -13,54 +12,68 @@
 // MobileMenu();
 
 function initAccordion() {
-  const accordionList = document.querySelectorAll('.js-accordion dt');
-  const activeClass = 'ativo'
-  
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
 
-  if(accordionList.length) {
-      accordionList[0].classList.add(activeClass); // adiciono a classe ativo no primeiro item do dt
-      accordionList[0].nextElementSibling.classList.add(activeClass); // adiciono a classe ativo no primeiro item do dd
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass); // adiciono a classe ativo no primeiro item do dt
+    accordionList[0].nextElementSibling.classList.add(activeClass); // adiciono a classe ativo no primeiro item do dd
 
-      function activeAccordion() {
-          this.classList.toggle(activeClass) // this serve tambem como const accordionlist, adicionando o toggle const do ativo no dt
-          this.nextElementSibling.classList.toggle(activeClass) // adicionando tambem no dd
-      }
+    function activeAccordion() {
+      this.classList.toggle(activeClass); // this serve tambem como const accordionlist, adicionando o toggle const do ativo no dt
+      this.nextElementSibling.classList.toggle(activeClass); // adicionando tambem no dd
+    }
 
-      accordionList.forEach((item) =>{
-          item.addEventListener('click', activeAccordion); // colocando o click e a funcao em cada dt e dd
-      });
-  };
-};
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion); // colocando o click e a funcao em cada dt e dd
+    });
+  }
+}
 
 initAccordion();
 
 function initScrollSuave() {
-    const LinksInternos =  document.querySelectorAll('.js-menu a[href^="#"]');
+  const LinksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-    function scrollToSection(event) {
-        event.preventDefault();
-        const PegarHref = event.currentTarget.getAttribute('href');
-        const section = document.querySelector(PegarHref);
-        section.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+  function scrollToSection(event) {
+    event.preventDefault();
+    const PegarHref = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(PegarHref);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
 
-        // const topo = section.offsetTop;
-        // window.scrollTo({
-        //     top: topo,
-        //     behavior: 'smooth'
-        // });
-        
-    }
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //     top: topo,
+    //     behavior: 'smooth'
+    // });
+  }
 
-    LinksInternos.forEach((link) => {
-        link.addEventListener('click', scrollToSection)
-    })
+  LinksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initScrollSuave();
+
+function goToLink () {
+  const cardLink = document.querySelectorAll(".card");
+
+  function handleClick(event) {
+  const servico = event.currentTarget.dataset.servico;
+  const mensagem = encodeURIComponent(
+    `Olá, gostaria de fazer um orçamento de ${servico}.`
+  );
+  const urlWhatsApp = `https://wa.me//5513991519647?text=${mensagem}`;
+  window.open(urlWhatsApp);
+  }
+
+  cardLink.forEach((card) => {
+    card.addEventListener("click", handleClick);
+  });
 
 }
 
-initScrollSuave()
-
-
-
+goToLink();
