8ce732958cc37266d0b1cba45eb6ce2241b50b7b
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2023-12-01 23:19:11 -0300

    fix: servicos

--- js/script.js
+++ js/script.js
@@ -77,3 +77,27 @@ function goToLink () {
 }
 
 goToLink();
+
+
+function MouseHover () {
+  document.addEventListener("DOMContentLoaded", function () {
+    var imgOverlays = document.querySelectorAll('.img-overlay');
+
+      imgOverlays.forEach(function (imgOverlay) {
+        imgOverlay.addEventListener("mouseenter", function () {
+          var overlayText = this.querySelector('.overlay-text');
+          overlayText.style.opacity = '1';
+          });
+
+        imgOverlay.addEventListener("mouseleave", function () {
+          var overlayText = this.querySelector('.overlay-text');
+          overlayText.style.opacity = '0';
+        });
+      });
+    });
+}
+
+MouseHover();
+  
+
+
